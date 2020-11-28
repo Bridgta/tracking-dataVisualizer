53d3ea8babb998d12cf8388df70c98b470899302
Bridget Asser <bridgetasser@gmail.com>
2020-11-27 22:46:53 -0500

    info card to receive api data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+import styles from './Cards.module.css'
 
 
-const Cards = (props) => {
-    console.log(props);
+const Cards = ({confirmed, recovered, deaths, lastUpdate}) => {
     return (
-        <h1>Cards</h1>
+        <div className={styles.container}>
+            <Grid container spacing={3} justify="center">
+            <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                    <Typography variant="h5">Real data</Typography>
+                    <Typography color="textSecondary">Real date</Typography>
+                    <Typography variant="body2">Number of active cases</Typography>
+                </CardContent>
+            </Grid>
+            </Grid>
+            <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                    <Typography variant="h5">Real data</Typography>
+                    <Typography color="textSecondary">Real date</Typography>
+                    <Typography variant="body2">Number of recovered cases</Typography>
+                </CardContent>
+            </Grid>
+            <Grid item component={Card}>
+                <CardContent>
+                    <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+                    <Typography variant="h5">Real data</Typography>
+                    <Typography color="textSecondary">Real date</Typography>
+                    <Typography variant="body2">Number of deaths</Typography>
+                </CardContent>
+            </Grid>
+            
+        </div>
     )
 }
 
